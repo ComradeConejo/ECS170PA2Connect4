9bc8c1d08c90c1fceadf2cbbf439126c02367615
ComradeConejo <83886410+ComradeConejo@users.noreply.github.com>
2023-03-01 12:59:48 -0800

    another small fix to clean up the code and reduce repetiotion

--- PA2/players.py
+++ PA2/players.py
@@ -405,7 +405,6 @@ class alphaBetaAI(connect4Player):
 					if (env.board[row+2][col+2] == player):
 						if (env.board[row+3][col+3] == opponent):
 							score[5] += 1
-				if	(env.board[row][col] == opponent and env.board[row+1][col+1] == opponent):
 					if (env.board[row+2][col+2] == opponent):
 						if (env.board[row+3][col+3] == player):
 							score[5] += 1
@@ -430,7 +429,6 @@ class alphaBetaAI(connect4Player):
 					if (env.board[row-2][col+2] == player):
 						if (env.board[row-3][col+3] == opponent):
 							score[5] += 1
-				if	(env.board[row][col] == opponent and env.board[row-1][col+1] == opponent):
 					if (env.board[row-2][col+2] == opponent):
 						if (env.board[row-3][col+3] == player):
 							score[5] += 1

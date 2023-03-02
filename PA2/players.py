6cf32c6ba5d8f1d716ba6c0ac5312c985d39e136
ComradeConejo <georgezaets@gmail.com>
2023-03-01 23:26:34 -0800

    pok-o

--- PA2/players.py
+++ PA2/players.py
@@ -82,8 +82,7 @@ class stupidAI(connect4Player):
 class minimaxAI(connect4Player):
  
 	def play(self, env, move):
-		start_time = time.time()
-		max_depth = 1000
+		depth = 3
 		# Find legal moves
 		env = deepcopy(env)
 		env.visualize = False
@@ -94,18 +93,15 @@ class minimaxAI(connect4Player):
 		# Init fitness trackers, zeros at start, will be replaced with the nash equilibrium for each
 		vs = np.zeros(7)
 		# Play until told to stop
-		for depth in range(1, max_depth):
-			if time.time() - start_time > 0.5:
-				break
-			store = -math.inf
-			for i in indices:
-				#print(str(i))
-				env_copy = deepcopy(env)
-				self.simulateMove(env_copy, i, self.position)
-				value = self.minimax(env_copy, depth, False, i)
-				vs[i] = max(store, value)
-				move[:] = [np.argmax(vs)]
+		store = -math.inf
+		for i in indices:
+			#print(str(i))
+			env_copy = deepcopy(env)
+			self.simulateMove(env_copy, i, self.position)
+			value = self.minimax(env_copy, depth-1, False, i)
+			vs[i] = max(store, value)
 			move[:] = [np.argmax(vs)]
+		move[:] = [np.argmax(vs)]
 		print("I finished")
 
 
@@ -135,7 +131,7 @@ class minimaxAI(connect4Player):
 		#playerTwos, playerThrees, playerFours, blockTwos, blockThrees, blockFours
 		score = np.zeros(6)
 		for row in range(len(env.board)):
-			for col in range(len(env.board[row])-3):
+			for col in range(4):
 				if (env.board[row][col] == player and env.board[row][col+1] == player):
 					if (env.board[row][col+2] == player):
 						if (env.board[row][col+3] == player):
@@ -168,8 +164,8 @@ class minimaxAI(connect4Player):
 	def diagonalCombo(self,env, player, opponent):
 		#playerTwos, playerThrees, playerFours, blockTwos, blockThrees, blockFours
 		score = np.zeros(6)
-		for row in range(len(env.board)-3):
-			for col in range(len(env.board[row])-3):
+		for col in range(len(env.board[0])-3):
+			for row in range(len(env.board)-3):
 				if (env.board[row][col] == player and env.board[row+1][col+1] == player):
 
 					if (env.board[row+2][col+2] == player):
@@ -181,27 +177,26 @@ class minimaxAI(connect4Player):
 					else:
 						score[0] += 1
 				#Blocking
-				if	(env.board[row][col] == player and env.board[row-1][col+1] == opponent) or (env.board[row][col] == opponent and env.board[row+1][col+1] == player):
-					if (env.board[row+2][col]+2 == opponent):
+				if	(env.board[row][col] == player and env.board[row+1][col+1] == opponent) or (env.board[row][col] == opponent and env.board[row+1][col+1] == player):
+					if (env.board[row+2][col+2] == opponent):
 						if (env.board[row+3][col+3] == opponent):
 							score[5] += 1
 						else:
 							score[4] += 1
-				if	(env.board[row][col] == opponent and env.board[row-1][col+1] == opponent):
-					if (env.board[row+2][col]+2 == player):
+				if	(env.board[row][col] == opponent and env.board[row+1][col+1] == opponent):
+					if (env.board[row+2][col+2] == player):
 						if (env.board[row+3][col+3] == opponent):
 							score[5] += 1
 						else:
 							score[4] += 1
-				if	(env.board[row][col] == opponent and env.board[row-1][col+1] == opponent):
-					if (env.board[row+2][col]+2 == opponent):
+				if	(env.board[row][col] == opponent and env.board[row+1][col+1] == opponent):
+					if (env.board[row+2][col+2] == opponent):
 						if (env.board[row+3][col+3] == player):
 							score[5] += 1
 						else:
 							score[4] += 1
 		
-		for row in range(3,6):
-			for col in range(len(env.board[row])-3):
+			for row in range(3,6):
 				if (env.board[row][col] == player and env.board[row-1][col+1] == player):
 
 					if (env.board[row-2][col+2] == player):
@@ -213,19 +208,19 @@ class minimaxAI(connect4Player):
 						score[0] += 1
 				#Blocking
 				if	(env.board[row][col] == player and env.board[row-1][col+1] == opponent) or (env.board[row][col] == opponent and env.board[row-1][col+1] == player):
-					if (env.board[row-2][col]+2 == opponent):
+					if (env.board[row-2][col+2] == opponent):
 						if (env.board[row-3][col+3] == opponent):
 							score[5] += 1
 						else:
 							score[4] += 1
 				if	(env.board[row][col] == opponent and env.board[row-1][col+1] == opponent):
-					if (env.board[row-2][col]+2 == player):
+					if (env.board[row-2][col+2] == player):
 						if (env.board[row-3][col+3] == opponent):
 							score[5] += 1
 						else:
 							score[4] += 1
 				if	(env.board[row][col] == opponent and env.board[row-1][col+1] == opponent):
-					if (env.board[row-2][col]+2 == opponent):
+					if (env.board[row-2][col+2] == opponent):
 						if (env.board[row-3][col+3] == player):
 							score[5] += 1
 						else:
@@ -247,13 +242,13 @@ class minimaxAI(connect4Player):
 		opponentCombo = np.zeros(6)	
 		playerCombo = self.combo(env, player, opponent)
 		opponentCombo = self.combo(env, opponent, player)
-		#1. 9999999999	win
-		#2. 100000	block opponent win
-  		#3. 500		build 3
-    	#4. 500		block 3
-    	#5. 10		block 2
-		#6. 5		build 2
-		return (playerCombo[2] * 999999999 + playerCombo[5] * 100000 + playerCombo[1] * 500 + playerCombo[4] * 500 + playerCombo[3] * 10 + playerCombo[0]*5) - (opponentCombo[2] * 999999999 + opponentCombo[5] * 100000  + opponentCombo[1] * 500 + opponentCombo[4] * 500 + opponentCombo[3] * 10 + opponentCombo[0]*5)
+		#1. 1000000000		win
+		#2. 100000		block opponent win
+  		#3. 10000		build 3
+    	#4. 1000		block 3
+    	#5. 250			block 2
+		#6. 50			build 2
+		return ((playerCombo[2] * 1000000000 + playerCombo[5] * 100000 + playerCombo[1] * 10000 + playerCombo[4] * 1000 + playerCombo[3] * 250 + playerCombo[0]*50) - (opponentCombo[2] * 1000000000 + opponentCombo[5] * 100000  + opponentCombo[1] * 10000 + opponentCombo[4] * 1000 + opponentCombo[3] * 250 + opponentCombo[0]*50))
 
 	def minimax(self, env, depth, maxPlayer, move):
 
@@ -294,12 +289,12 @@ class minimaxAI(connect4Player):
 		print("SIGTERM ENCOUNTERED")
 		sys.exit(0)
   
-  
 class alphaBetaAI(connect4Player):
  
 	def play(self, env, move):
-		start_time = time.time()
-		max_depth = 7
+		start = time.time()
+		random.seed(self.seed)
+		max_depth = random.randint(3, 20)
 		# Find legal moves
 		env = deepcopy(env)
 		env.visualize = False
@@ -311,7 +306,7 @@ class alphaBetaAI(connect4Player):
 		vs = np.zeros(7)
 		# Play until told to stop
 		for depth in range(1, max_depth):
-			if time.time() - start_time > 0.5:
+			if time.time() - start > 0.5:
 				break
 			store = -math.inf
 			alpha = -math.inf
@@ -319,15 +314,13 @@ class alphaBetaAI(connect4Player):
 			for i in indices:
 				child = deepcopy(env)
 				self.simulateMove(child, i, self.position)
-				value = self.alphaBeta(child, depth, False, i, alpha, beta)
-				value = max(store, value)
+				value = self.alphaBeta(child, depth-1, False, i, alpha, beta)
+				vs[i] = max(store, value)
 				alpha = max(alpha, value)
 				if alpha >= beta:
 					break
-				vs[i] = value
-				move[:] = [np.argmax(vs)]
 			move[:] = [np.argmax(vs)]
-		print("I finished")
+		move[:] = [np.argmax(vs)]
 
 	def verticalCombo(self,env,player, opponent):
 		#playerTwos, playerThrees, playerFours, blockTwos, blockThrees, blockFours
@@ -355,7 +348,7 @@ class alphaBetaAI(connect4Player):
 		#playerTwos, playerThrees, playerFours, blockTwos, blockThrees, blockFours
 		score = np.zeros(6)
 		for row in range(len(env.board)):
-			for col in range(len(env.board[row])-3):
+			for col in range(4):
 				if (env.board[row][col] == player and env.board[row][col+1] == player):
 					if (env.board[row][col+2] == player):
 						if (env.board[row][col+3] == player):
@@ -370,18 +363,14 @@ class alphaBetaAI(connect4Player):
 							score[5] += 1
 						else:
 							score[4] += 1
-					else:
-							score[3] += 1
-			#CHECK ALL HORIZONTAL BLOCKS
-			for col in range(3, len(env.board[row])):
+
+			for col in range(3, 7):
 				if	(env.board[row][col] == player and env.board[row][col-1] == opponent) or (env.board[row][col] == opponent and env.board[row][col-1] == player):
 					if (env.board[row][col-2] == opponent):
 						if (env.board[row][col-3] == opponent):
 							score[5] += 1
 						else:
 							score[4] += 1
-					else:
-						score[3] += 1
 
 			for col in range(1,6):
 				if	(env.board[row][col+1] == opponent and env.board[row][col] == player and env.board[row][col-1] == opponent):
@@ -392,8 +381,8 @@ class alphaBetaAI(connect4Player):
 	def diagonalCombo(self,env, player, opponent):
 		#playerTwos, playerThrees, playerFours, blockTwos, blockThrees, blockFours
 		score = np.zeros(6)
-		for row in range(len(env.board)-3):
-			for col in range(len(env.board[row])-3):
+		for col in range(len(env.board[0])-3):
+			for row in range(len(env.board)-3):
 				if (env.board[row][col] == player and env.board[row+1][col+1] == player):
 
 					if (env.board[row+2][col+2] == player):
@@ -409,16 +398,22 @@ class alphaBetaAI(connect4Player):
 					if (env.board[row+2][col+2] == opponent):
 						if (env.board[row+3][col+3] == opponent):
 							score[5] += 1
+						else:
+							score[4] += 1
 				if	(env.board[row][col] == opponent and env.board[row+1][col+1] == opponent):
 					if (env.board[row+2][col+2] == player):
 						if (env.board[row+3][col+3] == opponent):
 							score[5] += 1
+						else:
+							score[4] += 1
+				if	(env.board[row][col] == opponent and env.board[row+1][col+1] == opponent):
 					if (env.board[row+2][col+2] == opponent):
 						if (env.board[row+3][col+3] == player):
 							score[5] += 1
+						else:
+							score[4] += 1
 		
-		for row in range(3,6):
-			for col in range(len(env.board[row])-3):
+			for row in range(3,6):
 				if (env.board[row][col] == player and env.board[row-1][col+1] == player):
 
 					if (env.board[row-2][col+2] == player):
@@ -433,13 +428,20 @@ class alphaBetaAI(connect4Player):
 					if (env.board[row-2][col+2] == opponent):
 						if (env.board[row-3][col+3] == opponent):
 							score[5] += 1
+						else:
+							score[4] += 1
 				if	(env.board[row][col] == opponent and env.board[row-1][col+1] == opponent):
 					if (env.board[row-2][col+2] == player):
 						if (env.board[row-3][col+3] == opponent):
 							score[5] += 1
+						else:
+							score[4] += 1
+				if	(env.board[row][col] == opponent and env.board[row-1][col+1] == opponent):
 					if (env.board[row-2][col+2] == opponent):
 						if (env.board[row-3][col+3] == player):
 							score[5] += 1
+						else:
+							score[4] += 1
 		
 		return score
 
@@ -457,18 +459,16 @@ class alphaBetaAI(connect4Player):
 		opponentCombo = np.zeros(6)	
 		playerCombo = self.combo(env, player, opponent)
 		opponentCombo = self.combo(env, opponent, player)
-		#1. 9999999999	win
-		#2. 100000	block opponent win
-  		#3. 500		build 3
-    	#4. 500		block 3
-    	#5. 10		block 2
-		#6. 5		build 2
-		return (playerCombo[2] * 999999999 + playerCombo[5] * 100000 + playerCombo[1] * 500 + playerCombo[4] * 500 + playerCombo[3] * 10 + playerCombo[0]*5) - (opponentCombo[2] * 999999999 + opponentCombo[5] * 100000  + opponentCombo[1] * 500 + opponentCombo[4] * 500 + opponentCombo[3] * 10 + opponentCombo[0]*5)
-
-
+		#1. 1000000000	win
+		#2. 100000		block opponent win
+  		#3. 10000		build 3
+    	#4. 1000		block 3
+    	#5. 250			block 2
+		#6. 50			build 2
+		return ((playerCombo[2] * 1000000000 + playerCombo[5] * 100000 + playerCombo[1] * 10000 + playerCombo[4] * 1000 + playerCombo[3] * 250 + playerCombo[0]*50) - (opponentCombo[2] * 1000000000 + opponentCombo[5] * 100000  + opponentCombo[1] * 10000 + opponentCombo[4] * 1000 + opponentCombo[3] * 250 + opponentCombo[0]*50))
 
 	def alphaBeta(self, env, depth, maxPlayer, move, alpha, beta):
-		if maxPlayer:	
+		if maxPlayer:
 			position = self.position
 			bestValue = -math.inf
 		else:
@@ -480,8 +480,6 @@ class alphaBetaAI(connect4Player):
 		indices = []
 		for i, p in enumerate(possible):
 			if p: indices.append(i)
-		#If depth = 0 or game over on next move
-		#POTENTIAL PROBLEM 228 - 229
 
 		if depth == 0 or env.gameOver(move, self.position) or env.gameOver(move, self.opponent.position):
 			return self.evaluation(env, self.position, self.opponent.position)
@@ -490,12 +488,12 @@ class alphaBetaAI(connect4Player):
 			child = deepcopy(env)
 			self.simulateMove(child, i, position)
 			value = self.alphaBeta(child, depth - 1, not maxPlayer, i, alpha, beta)
-			if maxPlayer:	#MAX
+			if maxPlayer:
 				bestValue = max(bestValue, value)
 				alpha = max(alpha, bestValue)
 				if alpha >= beta:
 					break
-			else:			#MIN
+			else:
 				bestValue = min(bestValue, value)
 				beta = min(beta, bestValue)
 				if alpha >= beta:
